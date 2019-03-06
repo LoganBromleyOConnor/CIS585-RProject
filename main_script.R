fc6518856e53b01abc3f07b374cd24a9dd2069ce
Logan Bromley OConnor <loganboconnor@gmail.com>
2019-03-06 16:44:52 -0600

    Started data retrival process

--- main_script.R
+++ main_script.R
@@ -1,1 +1,29 @@
-# This is a test
+require(rvest)
+require(jsonlite)
+
+steamappid <- "294100"
+
+url <- paste('https://store.steampowered.com/appreviews/',steamappid,'?json=1', sep = '')
+reviewoffset <- "&start_offset="
+reviewoffsetvalue <- 0
+reviewloopbool = FALSE
+
+  while(reviewloopbool = FALSE)
+  {
+    newurl <- paste(url,)
+    
+    
+  }
+
+
+
+
+reviewdata_raw <- read_html(url)
+
+reviewdata_raw <- reviewdata_raw %>% html_nodes('body') %>% html_nodes('p') %>% html_text()
+reviewdata_df <- fromJSON(reviewdata_raw)
+reviewdata_df
+reviewdata_summary <- reviewdata_df[["query_summary"]]
+reviewdata_df <- reviewdata_df[["reviews"]]
+reviewdata_summary
+reviewdata_df
