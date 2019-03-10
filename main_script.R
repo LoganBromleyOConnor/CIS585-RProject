7c2672cc3dcb4698484b27a1d8725613be638c81
Logan Bromley OConnor <loganboconnor@gmail.com>
2019-03-09 23:12:02 -0600

    Created method for gathering all review data from Steam API and combining it into a single data frame. Then seperated the data into seperate data frames based on if they were negative or positive.

--- main_script.R
+++ main_script.R
@@ -2,28 +2,30 @@ require(rvest)
 require(jsonlite)
 
 steamappid <- "294100"
+url <- paste0('https://store.steampowered.com/appreviews/',steamappid,'?json=1&num_per_page=100&day_range=9223372036854775807&filter=all&language=english&review_type=all&purchase_type=all&start_offset=')
+pages <- list()
+i <- 0
 
-url <- paste('https://store.steampowered.com/appreviews/',steamappid,'?json=1', sep = '')
-reviewoffset <- "&start_offset="
-reviewoffsetvalue <- 0
-reviewloopbool = FALSE
-
-  while(reviewloopbool = FALSE)
+while(i < 9223372036854775807){
+  
+  rawjson <- fromJSON(paste0(url,i))
+  
+  message("Retrieving review ", i)
+  
+  if (rawjson[["query_summary"]][["num_reviews"]] == 0)
   {
-    newurl <- paste(url,)
-    
-    
+    message("Done")
+    break
   }
+  
+  pages[[i+1]] <- rawjson$reviews
+  i <- i+100
+  
+}
+reviewdata_df <- rbind_pages(pages)
+reviewdata_df<- unique(reviewdata_df)
+
+negreview_df <- reviewdata_df[reviewdata_df$voted_up == FALSE,]
+posreview_df <- reviewdata_df[reviewdata_df$voted_up == TRUE,]
 
 
-
-
-reviewdata_raw <- read_html(url)
-
-reviewdata_raw <- reviewdata_raw %>% html_nodes('body') %>% html_nodes('p') %>% html_text()
-reviewdata_df <- fromJSON(reviewdata_raw)
-reviewdata_df
-reviewdata_summary <- reviewdata_df[["query_summary"]]
-reviewdata_df <- reviewdata_df[["reviews"]]
-reviewdata_summary
-reviewdata_df
