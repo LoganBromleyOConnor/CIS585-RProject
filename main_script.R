10646464f112000c78449c69e3e207f98ce58096
Logan Bromley OConnor <loganboconnor@gmail.com>
2019-03-13 21:41:46 -0500

    - Turned review data into a corpus to clean text using tm package.
    - Turned corpus into Term-Document Matrix to find the frequency of words in each set of reviews.
    
    - Displayed frequent terms using ggwordcloud.

--- main_script.R
+++ main_script.R
@@ -2,21 +2,24 @@
 
 require(rvest)
 require(jsonlite)
+require(tm)
+require(ggwordcloud)
 
 #-----------------------------
 
 #Getting data
 
 steamappid <- "294100" #ID of the game/app in Steam
-url <- paste0('https://store.steampowered.com/appreviews/',steamappid,'?json=1&num_per_page=100&day_range=9223372036854775807&filter=all&language=english&review_type=all&purchase_type=all&start_offset=')
+url <- paste0('https://store.steampowered.com/appreviews/',steamappid,'?json=1&filter=all&language=english&num_per_page=100&start_offset=')
 pages <- list() #The list that the raw JSON will be stored in
 i <- 0 #The starting offset value for the API request
+amount <- 10000 #amount of reviews
 
-while(i < 9223372036854775807){
+while(i < amount){
   
   rawjson <- fromJSON(paste0(url,i)) #Getting JSON
   
-  message("Retrieving review ", i)
+  message("Retrieving review ", i, " out of ", amount, ".")
   
   if (rawjson[["query_summary"]][["num_reviews"]] == 0) #Stopping the loop when there are no reviews left
   {
@@ -37,3 +40,59 @@ reviewdata_df<- unique(reviewdata_df)#Removing any duplicate entries that might 
 negreview_df <- reviewdata_df[reviewdata_df$voted_up == FALSE,] #Data frame of negative reviews
 posreview_df <- reviewdata_df[reviewdata_df$voted_up == TRUE,] #Data frame of positive reviews
 #-----------------------------
+rm(pages, rawjson, reviewdata_df) #Removing unused data to clear up memory
+
+#Text mining reviews
+
+posreviews <- posreview_df$review #Getting only the review part of the data frame
+negreviews <- negreview_df$review
+
+rm(posreview_df, negreview_df) #Removing unused data to clear up memory
+
+posreviews <- VCorpus(VectorSource(posreviews)) #Creating Corpus of the reviews
+negreviews <- VCorpus(VectorSource(negreviews))
+
+clean_corpus <- function(corpus){  #Function to clean the corpus 
+  corpus <- tm_map(corpus, stripWhitespace) #Getting rid of unwanted white space
+  corpus <- tm_map(corpus, removePunctuation) #Getting rid of punctuation
+  corpus <- tm_map(corpus, content_transformer(tolower)) #making all characters lowercase
+  corpus <- tm_map(corpus, removeWords, c("game", "review", "like", "can", "theres", 
+                                          "just", "even", "games", "really", "1010", "dont",
+                                          "will", "ive", "one", "along", "doesnt", "well", 
+                                          "much", "many", "also", "lets", "isnt")) #List of unwanted words
+  corpus <- tm_map(corpus, removeWords, stopwords("en")) #Removing stopwords
+  return(corpus) #Returning the cleaned Corpus
+}
+
+
+posreviews <- clean_corpus(posreviews) #Applying the corpus function to the review corpuses
+negreviews <- clean_corpus(negreviews)
+
+find_freq_words <- function(corpus){ #Funcion to find the frequency of each word
+  
+  dtm <- TermDocumentMatrix(corpus) #Turning the corpus into a TermDocumentMatrix
+  m <- as.matrix(dtm)
+  v <- sort(rowSums(m),decreasing=TRUE)
+  d <- data.frame(word = names(v),freq=v) #Creating a data frame of the terms and their frequencies
+  return(d) #Returning data frame
+  
+}
+
+posreviewfreq <- find_freq_words(posreviews) #Applying frequency function 
+negreviewfreq <- find_freq_words(negreviews)
+
+wc_min_freq <- 5 #Variable to adjust the minimum frequency required to be displayed on the word cloud
+wc_max_words <- 30 #Variable to adjust the maxium amount of words to be displayed on the word cloud
+
+create_wordcloud <- function(wordfreq, wc_min_freq, wc_max_words){ #Function to create word cloud
+  
+  set.seed(78)
+  ggwordcloud(wordfreq$word, wordfreq$freq, min.freq = wc_min_freq, max.words = wc_max_words)
+  
+}
+
+poswordcloud <- create_wordcloud(posreviewfreq, wc_min_freq, wc_max_words) #Creating word cloud
+poswordcloud #Displaying word cloud
+
+negwordcloud <- create_wordcloud(negreviewfreq, wc_min_freq, wc_max_words)
+negwordcloud
