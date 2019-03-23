b0ff66075cf32f0cb3ad366e1e11f1818d2e83fd
Logan O'Connor <loganboconnor@gmail.com>
2019-03-23 00:58:25 -0500

    -Using disinct() from dplyr package rather than unique() to remove duplicate entries. Before unique() would not remove duplicate entires properly.
    -Added function to remove emojis and other non-text characters from review data.
    
    -Added more filter words.

--- main_script.R
+++ main_script.R
@@ -1,25 +1,25 @@
 #Required packages
-
 require(rvest)
 require(jsonlite)
 require(tm)
 require(ggwordcloud)
-
+require(dplyr)
 #-----------------------------
 
 #Getting data
 
-steamappid <- "294100" #ID of the game/app in Steam
+steamappid <- "730" #ID of the game/app in Steam
 url <- paste0('https://store.steampowered.com/appreviews/',steamappid,'?json=1&filter=all&language=english&num_per_page=100&start_offset=')
 pages <- list() #The list that the raw JSON will be stored in
 i <- 0 #The starting offset value for the API request
-amount <- 10000 #amount of reviews
+amount <- 1000 #amount of reviews
+numofentries <- 0
 
-while(i < amount){
+while(numofentries < amount){
   
   rawjson <- fromJSON(paste0(url,i)) #Getting JSON
   
-  message("Retrieving review ", i, " out of ", amount, ".")
+  message("Retrieving review ", numofentries, " out of ", amount, ".")
   
   if (rawjson[["query_summary"]][["num_reviews"]] == 0) #Stopping the loop when there are no reviews left
   {
@@ -28,14 +28,16 @@ while(i < amount){
   }
   
   pages[[i+1]] <- rawjson$reviews #Storing JSON in list
+  reviewdata_df <- rbind_pages(pages) #Turning list into a data frame
+  reviewdata_df <- reviewdata_df %>% distinct(recommendationid, .keep_all = TRUE)#Removing any duplicate entries that might have snuck in
+  numofentries <- length(reviewdata_df$recommendationid)
+  
   i <- i+100 #Moving to next set of reviews
   
 }
-#-----------------------------
 
+#-----------------------------
 #Organizing data
-reviewdata_df <- rbind_pages(pages) #Turning list into a data frame
-reviewdata_df<- unique(reviewdata_df)#Removing any duplicate entries that might have snuck in
 
 negreview_df <- reviewdata_df[reviewdata_df$voted_up == FALSE,] #Data frame of negative reviews
 posreview_df <- reviewdata_df[reviewdata_df$voted_up == TRUE,] #Data frame of positive reviews
@@ -46,6 +48,8 @@ rm(pages, rawjson, reviewdata_df) #Removing unused data to clear up memory
 
 posreviews <- posreview_df$review #Getting only the review part of the data frame
 negreviews <- negreview_df$review
+posreviews <- iconv(posreviews, 'UTF-8', 'ASCII') #Removing emojis or other non-text characters
+negreviews <- iconv(negreviews, 'UTF-8', 'ASCII')
 
 rm(posreview_df, negreview_df) #Removing unused data to clear up memory
 
@@ -53,13 +57,15 @@ posreviews <- VCorpus(VectorSource(posreviews)) #Creating Corpus of the reviews
 negreviews <- VCorpus(VectorSource(negreviews))
 
 clean_corpus <- function(corpus){  #Function to clean the corpus 
+  
   corpus <- tm_map(corpus, stripWhitespace) #Getting rid of unwanted white space
   corpus <- tm_map(corpus, removePunctuation) #Getting rid of punctuation
   corpus <- tm_map(corpus, content_transformer(tolower)) #making all characters lowercase
   corpus <- tm_map(corpus, removeWords, c("game", "review", "like", "can", "theres", 
                                           "just", "even", "games", "really", "1010", "dont",
                                           "will", "ive", "one", "along", "doesnt", "well", 
-                                          "much", "many", "also", "lets", "isnt")) #List of unwanted words
+                                          "much", "many", "also", "lets", "isnt", "good", "bad", 
+                                          "not", "get", "great", "play", "playing", "played")) #List of unwanted words
   corpus <- tm_map(corpus, removeWords, stopwords("en")) #Removing stopwords
   return(corpus) #Returning the cleaned Corpus
 }
